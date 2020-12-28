6e601c5b5978a5ec6d52f5beb63d190c638e18c3
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2020-12-28 15:49:03 +0600

    Homework
    Homework

---
+++ PaternsHomework/PaternsHomework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PaternsHomework
+{
+    /*Создать базовый класс Автомобиль
+    Создать потомки классы: автобус, грузовик, трактор
+    реализовать методы (ехать, ломаться)
+    свойства (вес, грузоподъемность)
+    наследование провести через abstract class и через интерфейс
+    привести пример переопределния любого метода*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus = new Bus();
+            bus.Go();
+            bus.Break();
+
+            Tractor tractor = new Tractor();
+            tractor.Break();
+            tractor.Go();
+        }
+    }
+}
---
+++ PaternsHomework/PaternsHomework/Tractor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaternsHomework
+{
+    public class Tractor : Car
+    {
+        public override void Break()
+        {
+            Console.WriteLine("Упс сломался");
+        }
+
+        public override void Go()
+        {
+            Console.WriteLine("Чучух ту ту");
+        }
+    }
+}
